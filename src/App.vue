cfa1129421d70dd1b81e6b24cee3ae242b010ae5
Magnetkopf <82427906+Magnetkopf@users.noreply.github.com>
2026-01-31 19:20:18 +0800

    chore: bootstrap project

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex flex-col items-center justify-center h-screen">
+    <h1 class="text-3xl">Helloworld</h1>
+    <p class="text-zinc-500">NodeGet Board</p>
+  </div>
+</template>
+
+<style scoped></style>
